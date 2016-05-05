2e0ba8f1ad6af374e9121ab493268cca15c2d98d
Jed Watson <jed.watson@me.com>
2016-05-06 01:14:06 +1000

    Updating to v1.3.8

--- src/helpers/color.js
+++ src/helpers/color.js
@@ -1,48 +1,49 @@
-'use strict';
+'use strict' /* @flow */
 
-var tinycolor = require('../../modules/tinycolor2');
+import tinycolor from '../../modules/tinycolor2'
 
-module.exports = {
+export default {
 
-  simpleCheckForValidColor: function(data) {
-    var keysToCheck = ['r', 'g', 'b', 'a', 'h', 's', 'a', 'v'];
-    var checked = 0;
-    var passed = 0;
+  simpleCheckForValidColor: function(data: any): any {
+    var keysToCheck = ['r', 'g', 'b', 'a', 'h', 's', 'a', 'v']
+    var checked = 0
+    var passed = 0
     for (var i = 0; i < keysToCheck.length; i++) {
-      var letter = keysToCheck[i];
+      var letter = keysToCheck[i]
       if (data[letter]) {
-        checked++;
+        checked++
         if (!isNaN(data[letter])) {
-          passed++;
+          passed++
         }
       }
     }
 
     if (checked === passed) {
-      return data;
+      return data
     }
   },
 
-  toState: function(data, oldHue) {
-    var color = tinycolor(data);
-    var hsl = color.toHsl();
-    var hsv = color.toHsv();
+  toState: function(data: any, oldHue: number): any {
+    var color = data.hex ? tinycolor(data.hex) : tinycolor(data)
+    var hsl = color.toHsl()
+    var hsv = color.toHsv()
     if (hsl.s === 0) {
-      hsl.h = oldHue;
-      hsv.h = oldHue;
+      hsl.h = oldHue || 0
+      hsv.h = oldHue || 0
     }
 
     return {
       hsl: hsl,
-      hex: color.toHex(),
+      hex: `#${ color.toHex() }`,
       rgb: color.toRgb(),
       hsv: hsv,
       oldHue: data.h || oldHue || hsl.h,
-    };
+      source: data.source,
+    }
   },
 
-  isValidHex: function(hex) {
-    return tinycolor(hex).isValid();
+  isValidHex: function(hex: string): boolean {
+    return tinycolor(hex).isValid()
   },
 
-};
+}
